3e1765442cf012d121fb8b7ee91d897562be7bbc
Gabriel Veloso <agnans92@gmail.com>
2014-09-12 23:00:50 -0300

    1008 Salary

---
+++ 1-BEGINNER/1008.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, hours;
+    float hourlyRate;
+
+    cin >> n >> hours >> hourlyRate;
+    cout << "NUMBER = " << n << endl;
+    cout << "SALARY = U$ " << hours * hourlyRate << endl;
+}
